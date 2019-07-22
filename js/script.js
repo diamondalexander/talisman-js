2e59147f2030f71ff88d94c52096ad519ca754fa
Diamond Alexander <diamondnalexander@gmail.com>
2019-07-21 23:44:48 -0600

    Updates to UI and Toggle Functionality

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// Toggle for Main Navigation -> Checkin Navigation
+
+function hideMainNav() {
+  var main = document.getElementById('mainNav');
+  main.classList.toggle('hidden');
+  var checkin = document.getElementById('checkinNav');
+  checkin.classList.remove('hidden');
+};
+
+
+
+
+
